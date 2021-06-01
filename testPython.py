d2e29d679b658dce10bd7f7cf91d559c2393ca2b
Erica <erica_dematteis@yahoo.it>
2021-06-01 10:52:42 +0200

    aggiunta azioni MSFT

--- testPython.py
+++ testPython.py
@@ -8,5 +8,15 @@ dati=t.history(period="10Y")
 
 dati.to_csv("AzioniApple10Anni.csv")
 
-print("Applicazione che si connette a YF per scaricare i dati di Apple")
+print("Applicazione che si connette a YF per scaricare i dati di Microsoft")
+print(dati.head())
+
+
+t=yf.Ticker("MSFT")
+
+dati_msft=t.history(period="10Y")
+
+dati.to_csv("AzioniMSFT10Anni.csv")
+
+print("Applicazione che si connette a YF per scaricare i dati di Microsoft")
 print(dati.head())
